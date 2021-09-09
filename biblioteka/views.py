b3d7316362d13d7c60357f8c37e64f13e993bf25
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-09 15:49:02 +0200

    mails

--- biblioteka/views.py
+++ biblioteka/views.py
@@ -1,6 +1,6 @@
 from biblioteka.models import Autor, Ksiazka
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 from .signals import nasz_signal
 from django.db import transaction
 from django.core.mail import send_mail
@@ -13,14 +13,32 @@ def glowny(request):
     dodaj_do_bazy(autor, ksiazka)
     return HttpResponse('to jest nasza glowna strona')
 
-def wysylanie_maila():
-    send_mail(
-        subject='temat',
-        message='link klikniety',
-        from_email='test@test.net',
-        recipient_list=['abc@abc.com'],
-        fail_silently=False
-    )
+def wysylanie_maila(request):
+    # send_mail(
+    #     subject='temat',
+    #     message='link klikniety',
+    #     from_email='test@test.net',
+    #     recipient_list=['abc@abc.com'],
+    #     fail_silently=False
+    # )
+
+    if request.method == 'POST':
+        if request.POST.get('email', False):
+            email = request.POST['email']
+            wiadomosc = 'form message' + email
+            send_mail(
+                subject='temat',
+                message=wiadomosc,
+                from_email=email,
+                recipient_list=['abc@abc.com'],
+                fail_silently=False
+            )
+            return HttpResponse('Mail zostal wyslany')
+               
+    return render(request, 'email_form.html')
+
+    # return HttpResponse('to wysle naszego maila')
+
 #transaction atomic stworz wszystko albo nic
 @transaction.atomic()
 def dodaj_do_bazy(autor, ksiazka):
