39ae515b03dee81646f27f135dcad7d805ba6cd6
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-09 13:57:35 +0200

    managers lekcja

--- biblioteka/models.py
+++ biblioteka/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from biblioteka.managers import KsiazkaManager
 
 class Autor(models.Model):
     imie = models.CharField(max_length=20, blank=False)
@@ -8,10 +9,26 @@ class Autor(models.Model):
     def __str__(self):
         return self.imie + " " + self.nazwisko
 
+class Gatunek():
+    NIEZNANY = 0
+    FANTASY = 1
+    HORROR = 2
+    DRAMA = 3
+    GATUNKI = (
+        (NIEZNANY, 'Nieznany'),
+        (FANTASY, 'Fantasy'),
+        (HORROR, 'Horror'),
+        (DRAMA, 'Drama'),
+    )
+        
 class Ksiazka(models.Model):
     tytul = models.CharField(max_length=50, blank=False)
     rok_wydania = models.IntegerField(blank=False)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='ksiazki')
+    gatunek = models.PositiveSmallIntegerField(choices=Gatunek.GATUNKI, default=0)
+
+    objects = models.Manager() # jesli customowy manager jest to defaultowy objects przestaje istniec wiec dodajemy
+    ksiazki = KsiazkaManager()
 
     def __str__(self):
         return self.tytul
