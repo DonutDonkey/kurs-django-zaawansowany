7d11c7d7b8df442e2a8115617a5b8b21ed18af05
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-09 15:01:30 +0200

    request i wlasne sygnaly

--- biblioteka/signals.py
+++ biblioteka/signals.py
@@ -1,6 +1,9 @@
 from biblioteka.models import Autor
-from django.dispatch import receiver
+from django.dispatch import receiver, Signal
 from django.db.models.signals import post_save, pre_save
+from django.core.signals import request_finished
+
+nasz_signal = Signal(providing_args=['imie'])
 
 # sender : co wywoławo funkcje
 # instance : obiekt który wywołał funkcje
@@ -16,3 +19,11 @@ def autor_po_zapisaniu(sender, instance, **kwargs):
         print(Autor.objects.get(id=instance.id).imie)
     except Autor.DoesNotExist:
         print('Nowy aktor')
+
+@receiver(request_finished)
+def strona_wczytana(sender, **kwargs):
+    print('Strona sie wczytala')
+
+@receiver(nasz_signal)
+def podane_imie(sender, **kwargs):
+    print('Podane imie: ', kwargs.get('imie'))
--- biblioteka/views.py
+++ biblioteka/views.py
@@ -1,3 +1,8 @@
+from biblioteka.models import Autor
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .signals import nasz_signal
 # Create your views here.
+def glowny(request):
+    nasz_signal.send(sender=Autor, imie='Admin')
+    return HttpResponse('to jest nasza glowna strona')
