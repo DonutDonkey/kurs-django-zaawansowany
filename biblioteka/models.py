7dbe3ae12faf2a92ce866a72e1549f45bf60316a
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-09 14:26:08 +0200

    signals

--- biblioteka/models.py
+++ biblioteka/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from biblioteka.managers import KsiazkaManager
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 
 class Autor(models.Model):
     imie = models.CharField(max_length=20, blank=False)
@@ -8,6 +10,18 @@ class Autor(models.Model):
 
     def __str__(self):
         return self.imie + " " + self.nazwisko
+# sender : co wywoławo funkcje
+# instance : obiekt który wywołał funkcje
+# **kwargs : argumenty w funkcji
+@receiver([post_save], sender=Autor)
+def autor_po_zapisaniu(sender, instance, **kwargs):
+    print('Autor saved')
+    print(instance.imie)
+@receiver([pre_save], sender=Autor)
+def autor_po_zapisaniu(sender, instance, **kwargs):
+    print('Autor before being saved')
+
+# post_save.connect(autor_po_zapisaniu, sender=Autor)
 
 class Gatunek():
     NIEZNANY = 0
@@ -40,12 +54,12 @@ class Ksiazka(models.Model):
         verbose_name = 'książka' #nazwa w bazie
         verbose_name_plural = 'książki' #nazwa w liczbie mnogiej
         #kombinacja tych wartosci musi byc unikalna
-        unique_together = ['tytul', 'rok wydania']
+        unique_together = ['tytul', 'rok_wydania']
         #indexowanie, przyspieszanie iteracji po databasie
-        indexes = [
-            models.Index(fields=['tytul'], name=['tytul_indx']),
-            models.Index(fields=['tytul', 'rok_wydania'], name=['tytul_indx'])
-        ]
+        # indexes = [
+        #     models.Index(fields=['tytul'], name=['tytul_indx']),
+        #     models.Index(fields=['tytul', 'rok_wydania'])
+        # ]
         permissions = [
             ('can_update_ksiazka', "Może zmieniać książke")
         ]
