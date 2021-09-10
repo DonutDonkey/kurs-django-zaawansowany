b77a9ab2b8e4621021f8dda6febae4de80771d1a
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-10 13:39:34 +0200

    functions and forms tests

--- biblioteka/tests.py
+++ biblioteka/tests.py
@@ -1,7 +1,10 @@
+from biblioteka.validators import validate_rok
 from biblioteka.models import Autor, Ksiazka
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from .views import nowy_form
+from django.core.exceptions import ValidationError
+from .forms import NaszForm
 
 # Create your tests here.
 class BibliotekaTests(TestCase):
@@ -45,3 +48,25 @@ class BibliotekaTests(TestCase):
     def test_ksiazki_manager(self) -> None:
         ksiazki = Ksiazka.ksiazki.nowoczesne()
         self.assertGreater(len(ksiazki),0)
+
+#Functions test
+    def test_funkcja_validate_rok(self):
+        self.assertRaises(ValidationError, validate_rok, 2025)
+
+    def test_funkcja_validate_rok(self):
+        self.assertEqual(validate_rok(2019), 2019)
+
+#FORMS test
+    def test_nasz_from_valid(self):
+        form = NaszForm(data={
+            'imie': 'Test',
+            'rok': 2020
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_nasz_from_valid(self):
+        form = NaszForm(data={
+            'imie': 'Test',
+            'rok': 2025
+        })
+        self.assertFalse(form.is_valid())
