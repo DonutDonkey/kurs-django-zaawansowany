e165b65831ab486b7981ae2b68fb0bdee09998ac
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-10 11:32:32 +0200

    form validator

--- biblioteka/forms.py
+++ biblioteka/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from django.core.validators import MaxValueValidator
 
 class NaszForm(forms.Form):
     imie = forms.CharField(label='Imie', max_length=20)
-    rok = forms.IntegerField()
+    rok = forms.IntegerField(blank=False, validators=[MaxValueValidator(2020)])
+
+    # def clean_rok(self):
+    #     rok = self.cleaned_data.get('rok')
+    #     if rok > 2020:
+    #         raise forms.ValidationError('Rok jest wiekszy niz 2020')
+    #     return rok
+
+    def clean(self):
+        cleaned_data = super(NaszForm, self).clean()
+        rok = cleaned_data.get('rok')
+        if rok > 2020:
+            raise forms.ValidationError('Rok jest wiekszy niz 2020')
+        return rok
