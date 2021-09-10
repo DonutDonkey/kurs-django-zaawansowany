1831a2f4569c4e7d69889867e13f6a26e28954d6
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-09-10 10:13:45 +0200

    custom logs

--- biblioteka/signals.py
+++ biblioteka/signals.py
@@ -1,9 +1,19 @@
+import logging
 from biblioteka.models import Autor
 from django.dispatch import receiver, Signal
 from django.db.models.signals import post_save, pre_save
 from django.core.signals import request_finished
 
 nasz_signal = Signal(providing_args=['imie'])
+
+autor_log = logging.getLogger('autor_log')
+autor_log.setLevel(logging.DEBUG)
+
+log_handler = logging.FileHandler('logs/autor.log')
+log_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_handler.setFormatter(formatter)
+autor_log.addHandler(log_handler)
 
 # sender : co wywoławo funkcje
 # instance : obiekt który wywołał funkcje
@@ -12,6 +22,8 @@ nasz_signal = Signal(providing_args=['imie'])
 def autor_po_zapisaniu(sender, instance, **kwargs):
     print('Autor saved')
     print(instance.imie)
+    autor_log.info('Autor zapisane przez : ' + instance.imie + ' ' + instance.nazwisko)
+
 @receiver([pre_save], sender=Autor)
 def autor_po_zapisaniu(sender, instance, **kwargs):
     print('Autor before being saved')
